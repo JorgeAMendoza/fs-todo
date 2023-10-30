91d1cca1c193a5597af68f31ebbdc3a4a213adbd
Jorge Mendoza <jorgemendoza2002@gmail.com>
2023-10-30 06:38:03 -0500

    add files to ensure that at least the frontend pre-commit works.

--- server/src/routes/login.ts
+++ server/src/routes/login.ts
@@ -6,11 +6,11 @@ router.post('/login', (req: Request, res: Response) => {
   // login logic here
   // auth should be done before reaching this route.
   res.json({ message: 'this is the login route' })
-})
+});
 
 router.post('/new-user', (req: Request, res: Response) => {
   // logic here to create a new user
   res.json({ message: 'this is the new user route' })
-})
+});
 
 export default router
